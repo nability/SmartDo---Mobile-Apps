0168c3de39ddad69d0d49fc060b78831a8a12ea0
Nabiel Fauzan <151288742+nability@users.noreply.github.com>
2026-02-09 10:57:27 +0700

    Remove Firebase config from firebase.ts
    Removed sensitive Firebase configuration details.

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,7 @@ import { getFirestore, Firestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDItL8huRYuw_dH-v79r9umbXpD6acXSA4",
-  authDomain: "projectuas-3d168.firebaseapp.com",
-  projectId: "projectuas-3d168",
-  storageBucket: "projectuas-3d168.firebasestorage.app",
-  messagingSenderId: "990878890216",
-  appId: "1:990878890216:web:199d544384c40d9f75f15d"
+
 };
 
 const app = initializeApp(firebaseConfig);
